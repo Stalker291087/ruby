e2bbf4b58e60cfbef66982f8b6e23f35b9e40046
Stalker291087 <jeancarloe01@hotmail.com>
2022-02-20 09:31:06 -0600

    Adding firt ruby file

---
+++ hola_mundo.rb
@@ -0,0 +1,5 @@
+print "Introduce tu nombre: "
+nombre = gets.chomp
+saludo = "buenas tardes"
+#puts "Hola " # Puts siempre va poner un salto de linea al final.
+puts "Hola #{nombre}, que tengas #{saludo}"
